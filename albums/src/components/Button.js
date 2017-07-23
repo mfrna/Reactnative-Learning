89923f1aa801def2a24b3a2b67d0e1bdefe6d1fd
mfrna <mfrna@mfrna.com>
2017-07-24 00:16:13 +0200

    Some progress

---
+++ albums/src/components/Button.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { Text, TouchableOpacity } from 'react-native';
+
+const Button = ({ onPress, children }) => {
+  const {ButtonStyle,TextStyle} = styles;
+  return (
+    <TouchableOpacity onPress={onPress} style={ButtonStyle}>
+      <Text style={TextStyle}>{children}</Text>
+    </TouchableOpacity>
+  );
+}
+
+const styles = {
+  ButtonStyle:{
+    flex:1,
+    alignSelf:'stretch',
+    backgroundColor:'#fff',
+    borderRadius:5,
+    borderWidth:1,
+    borderColor:'#007aff',
+    marginLeft:5,
+    marginRight:5
+  },
+  TextStyle:{
+    alignSelf:'center',
+    color:'#077aff',
+    fontSize:16,
+    fontWeight:'600',
+    paddingTop:10,
+    paddingBottom:10
+  }
+}
+
+export default Button;
